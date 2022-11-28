86e137d609a5d1bf537ee48e672e83d456e35875
brenekv.03 <brenekv.03@A321-15>
2022-11-28 12:28:34 +0100

    Přidejte soubory projektu.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace List09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "_bird-157249_960_720.png");
+            this.imageList1.Images.SetKeyName(1, "_canard-159511_640.png");
+            this.imageList1.Images.SetKeyName(2, "_cow-35561_640.png");
+            this.imageList1.Images.SetKeyName(3, "_dog-35553_640.png");
+            this.imageList1.Images.SetKeyName(4, "_elephant-35527_640.png");
+            this.imageList1.Images.SetKeyName(5, "_hippo-47726_640.png");
+            this.imageList1.Images.SetKeyName(6, "_chamois-162138_960_720.png");
+            this.imageList1.Images.SetKeyName(7, "_lion-304444_640.png");
+            this.imageList1.Images.SetKeyName(8, "_pig-47920_960_720.png");
+            this.imageList1.Images.SetKeyName(9, "_sheep-47527_960_720.png");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(408, 67);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(300, 300);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 255);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Bitmap> seznamObrazku = new List<Bitmap>();
+        int index;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            //ověřit že tam jsou stale obrazky
+            int pocet = imageList1.Images.Count-1;
+            index = random.Next(0, pocet);
+            if (pocet >= 0)
+            {
+                for(int i =0;i< pocet; i++)
+                {
+                    pictureBox1.Image = seznamObrazku[index];
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Projít kolekci v imageList1 a obrázky přidat do ListObrazku
+            foreach(Bitmap obrazek in imageList1.Images)
+            {
+                seznamObrazku.Add(obrazek);
+            }
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            //smazat obrázek v picture box(null)
+            seznamObrazku.Remove(seznamObrazku[index]);
+            pictureBox1.Image = null;
+            //smazat obrázek v seznamObrazku
+        }
+    }
+}
